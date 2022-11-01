2b150e7423446ab6b3c924362f53b0b5125869cf
Mostafa <M.GolMohammadi@emofid.com>
2022-11-01 22:13:42 +0100

    add repositories

---
+++ src/main/java/com/abn/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.abn.repository;
+
+import com.abn.entity.Role;
+import com.abn.enums.RoleType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(RoleType roleType);
+
+}
