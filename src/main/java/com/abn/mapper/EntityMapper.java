39b0e6d9bafddd10d792e8b9fba85705e62b27f2
Mostafa <M.GolMohammadi@emofid.com>
2022-11-01 22:30:34 +0100

    add mappers

--- src/main/java/com/abn/mapper/EntityMapper.java
+++ src/main/java/com/abn/mapper/EntityMapper.java
@@ -2,6 +2,11 @@ package com.abn.mapper;
 
 import java.util.List;
 
+/**
+ * @author Mostafa
+ * @version 1.0
+ * @since 2022-11-01
+ */
 public interface EntityMapper<D, E> {
     E toEntity(D dto);
 
